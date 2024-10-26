67c6751b9b65596a7a0894f153a8759c7da9503e
Heberth2107 <sba24147@student.cct.ie>
2024-10-26 21:26:55 +0100

    First Commit

---
+++ src/customervalidator/CustomerValidator.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package customervalidator;
+
+import entities.Customer;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import java.util.HashSet;
+import utils.StringFunctions;
+
+
+/**
+ *
+ * @author heber
+ */
+public class CustomerValidator {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    //read file
+//    try(BufferedReader buffer = new BufferedReader(new FileReader("C:\\Users\\heber\\Downloads\\customers.txt")))
+//    {
+//        //create variables
+//       
+//        }
+//        
+//    }
+//    catch(IOException e){
+//        System.out.println(e);
+//    }
+    
+        
+        
+   
+            
+    }
+    
+}
