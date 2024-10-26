37f5dad6a03023a50f03004436f1f537adad336d
Heberth2107 <sba24147@student.cct.ie>
2024-10-26 21:28:39 +0100

    Set Properties

--- src/customervalidator/CustomerValidator.java
+++ src/customervalidator/CustomerValidator.java
@@ -27,16 +27,48 @@ public class CustomerValidator {
     public static void main(String[] args) {
         // TODO code application logic here
     //read file
-//    try(BufferedReader buffer = new BufferedReader(new FileReader("C:\\Users\\heber\\Downloads\\customers.txt")))
-//    {
-//        //create variables
-//       
-//        }
-//        
-//    }
-//    catch(IOException e){
-//        System.out.println(e);
-//    }
+    try(BufferedReader buffer = new BufferedReader(new FileReader("C:\\Users\\heber\\Downloads\\customers.txt")))
+    {
+        //create variables
+        int lineCount = 0;
+        String line;
+        Customer customer = null;
+        //Start the loop to navigate between the lines
+        while((line = buffer.readLine()) != null){
+            
+            lineCount++;
+            //Set First name and Second name
+            if(lineCount == 1){
+                customer = new Customer();
+                //separate First Name and Second Name
+                customer.SetFirstName(line.split(" ")[0]);
+                customer.SetSecondName(line.split(" ")[1]);   
+            }
+            else if(lineCount == 2){   
+                //set Purchase 
+                //the value of purchase must be double
+                customer.SetPurchase(parseDouble(line));
+            }
+            else if(lineCount == 3){ 
+                //set class
+                customer.SetClass(parseInt(line));
+            }
+            else{
+                //set Last Purchase
+                customer.SetLastPurchase(parseInt(line));
+                lineCount = 0;
+            } 
+            
+            
+             
+            System.out.println(customer.GetFirstName() + " " + customer.GetSecondName());
+            System.out.println(customer.CalcularDesconto());
+        }
+        
+    }
+    catch(IOException e){
+        System.out.println(e);
+    }
     
         
         
